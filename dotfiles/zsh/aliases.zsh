5c2c54499cadd06087b4acd3a01b6ff5fc909c6a
Alexandre <75211316+Ekyoz@users.noreply.github.com>
2026-03-24 15:09:43 +0100

    Add zsh-update alias for configuration update
    Added an alias for updating the ZSH configuration.

--- dotfiles/zsh/aliases.zsh
+++ dotfiles/zsh/aliases.zsh
@@ -2,11 +2,14 @@
 # aliases.zsh — Aliases personnels
 # =============================================================================
 
-#------- MAIN -------#
+#------- ZSH -------#
 alias zsh-config='$EDITOR ~/.zshrc'
 alias zsh-alias='$EDITOR $ZSH_CUSTOM/aliases.zsh'
 alias zsh-macros='$EDITOR $ZSH_CUSTOM/macros.zsh'
 alias zsh-local='$EDITOR $ZSH_CUSTOM/local.zsh'
+alias zsh-update="curl -fsSL https://raw.githubusercontent.com/Ekyoz/Ekyoz/main/install.sh | bash"
+
+#------- MAIN -------#
 alias reload='source ~/.zshrc'
 
 #------ UTILITIES ------#
