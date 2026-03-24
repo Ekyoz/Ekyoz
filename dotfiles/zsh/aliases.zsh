5d332903dc25611fea3b7790fc5edfef8792ce14
Alexandre <75211316+Ekyoz@users.noreply.github.com>
2026-03-24 13:46:37 +0100

    Create aliases.zsh

---
+++ dotfiles/zsh/aliases.zsh
@@ -0,0 +1,14 @@
+# =============================================================================
+# aliases.zsh — Aliases personnels
+# =============================================================================
+ 
+#------- MAIN -------#
+alias zsh="nano ~/.zshrc"
+alias zsh-alias="nano $ZSH_CUSTOM/aliases.zsh"
+alias reload="source ~/.zshrc"
+ 
+#------ UTILITIES ------#
+alias cls=clear
+alias wipe="echo '' > "
+ 
+# Ajoute tes aliases ici 
