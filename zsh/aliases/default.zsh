0095a8641c71b713dfd09e3794eec4f7bf11ea64
Alexandre <75211316+Ekyoz@users.noreply.github.com>
2026-04-13 22:38:27 +0200

    Update ZSH aliases for local configurations

--- zsh/aliases/default.zsh
+++ zsh/aliases/default.zsh
@@ -4,11 +4,9 @@
 
 #------- ZSH -------#
 alias zsh-config='$EDITOR ~/.zshrc'
-alias zsh-alias='$EDITOR $ZSH_CUSTOM/aliases/default.zsh'
-alias zsh-alias-local='$EDITOR $ZSH_CUSTOM/aliases/local.zsh'
-alias zsh-macros='$EDITOR $ZSH_CUSTOM/macros/default.zsh'
-alias zsh-macros-local='$EDITOR $ZSH_CUSTOM/macros/local.zsh'
-alias zsh-local='$EDITOR $ZSH_CUSTOM/local.zsh'
+alias zsh-alias='$EDITOR $ZSH_CUSTOM/aliases/local.zsh'
+alias zsh-macros='$EDITOR $ZSH_CUSTOM/macros/local.zsh'
+alias zsh-export='$EDITOR $ZSH_CUSTOM/export.zsh'
 alias zsh-update="curl -fsSL https://raw.githubusercontent.com/Ekyoz/Ekyoz/main/install.sh | bash"
 
 #------- MAIN -------#
