918b08fb5e5e05c617061fac3a1ea64bacad2801
Alexandre <75211316+Ekyoz@users.noreply.github.com>
2026-05-09 14:56:47 +0200

    Update fzf.zsh

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -12,9 +12,9 @@ if command -v fd &>/dev/null; then
   _fzf_dirs='fd --type d --follow --exclude .git'
   _fzf_dirs_h='fd --type d --hidden --follow --exclude .git'
 else
-  _fzf_files='find . \( -name ".*" -prune \) -o \( -type f -not -path "*/.git/*" -print \)'
+  _fzf_files='find . -mindepth 1 -name ".*" -prune -o -type f -not -path "*/.git/*" -print'
   _fzf_files_h='find . -type f -not -path "*/.git/*"'
-  _fzf_dirs='find . \( -name ".*" -prune \) -o \( -type d -not -path "*/.git/*" -print \)'
+  _fzf_dirs='find . -mindepth 1 -name ".*" -prune -o -type d -not -path "*/.git/*" -print'
   _fzf_dirs_h='find . -type d -not -path "*/.git/*"'
 fi
 
