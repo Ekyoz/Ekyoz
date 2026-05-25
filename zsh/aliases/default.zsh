99b720f19363e0aa0d541103395f145e8ef56e94
Alexandre <75211316+Ekyoz@users.noreply.github.com>
2026-05-25 13:36:49 +0200

    Update zsh-update alias URL

--- zsh/aliases/default.zsh
+++ zsh/aliases/default.zsh
@@ -7,7 +7,7 @@ alias zsh-config='$EDITOR ~/.zshrc'
 alias zsh-alias='$EDITOR $ZSH_CUSTOM/aliases/local.zsh'
 alias zsh-macros='$EDITOR $ZSH_CUSTOM/macros/local.zsh'
 alias zsh-export='$EDITOR $ZSH_CUSTOM/export.zsh'
-alias zsh-update="curl -fsSL https://raw.githubusercontent.com/Ekyoz/Ekyoz/main/install.sh | bash"
+alias zsh-update="curl -fsSL https://zsh.coxyz.fr | bash"
 
 #------- MAIN -------#
 alias reload='source ~/.zshrc'
